6440ec74549212c4a88c9dba24d4316e0318f387
Axis93 <sim.pia.work@gmail.com>
2020-04-16 18:46:32 +0100

    Added CR-D of CRUD - missing update

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,38 @@
 class PlayersController < ApplicationController
-	def new
+	def index
+		@players = Player.all
 	end
 
-	def create
-		render plain: params[:players].inspect
+	def show
+		@player = Player.find(params[:id])
 	end
+
+	def new
+		@player = Player.new
+	end
+
+
+
+	def create
+		@player = Player.new(player_params)
+
+		if @player.save
+			redirect_to @player
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@player = Player.find(params[:id])
+		@player.destroy
+
+		redirect_to players_path
+	end
+
+	private
+		def player_params
+			params.require(:player).permit(:name, :win, :draw, :lose, 
+				:gs, :gt, :dr, :points)
+		end
 end
